b65c8b165a14335b0183f8b96d588c711b86b38f
Jacob Gold <jacobgaffney@me.com>
2018-05-23 15:48:34 +1000

    Initial
    Seems to chug along nicely.

---
+++ MMM-darksky-hourly.js
@@ -0,0 +1,322 @@
+Module.register("MMM-darksky-hourly", {
+
+  defaults: {
+    apiKey: "",
+    apiBase: "https://api.darksky.net/forecast",
+    units: config.units,
+    language: config.language,
+    twentyFourHourTime: true,
+    showPrecipitationPossibilityInRow: true,
+    showDayInRow: true,
+    showIconInRow: true,
+    updateInterval: 10 * 60 * 1000, // every 5 minutes
+    animationSpeed: 1000,
+    initialLoadDelay: 0, // 0 seconds delay
+    retryDelay: 2500,
+    tempDecimalPlaces: 0, // round temperatures to this many decimal places
+    geoLocationOptions: {
+      enableHighAccuracy: true,
+      timeout: 5000
+    },
+    latitude:  null,
+    longitude: null,
+    maxHoursForecast: 8,   // maximum number of days to show in forecast
+    unitTable: {
+      'default':  'auto',
+      'metric':   'si',
+      'imperial': 'us'
+    },
+    iconTable: {
+      'clear-day':           'wi-day-sunny',
+      'clear-night':         'wi-night-clear',
+      'rain':                'wi-rain',
+      'snow':                'wi-snow',
+      'sleet':               'wi-sleet',
+      'wind':                'wi-cloudy-gusts',
+      'fog':                 'wi-fog',
+      'cloudy':              'wi-cloudy',
+      'partly-cloudy-day':   'wi-day-cloudy',
+      'partly-cloudy-night': 'wi-night-alt-cloudy',
+      'hail':                'wi-hail',
+      'thunderstorm':        'wi-thunderstorm',
+      'tornado':             'wi-tornado'
+    },
+
+    debug: false
+  },
+
+  getTranslations: function () {
+    return false;
+  },
+
+  getScripts: function () {
+    return [
+      'jsonp.js',
+      'moment.js'
+    ];
+  },
+
+  getStyles: function () {
+    return ["weather-icons.css", "MMM-darksky-hourly.css"];
+  },
+
+  shouldLookupGeolocation: function () {
+    return this.config.latitude == null &&
+           this.config.longitude == null;
+  },
+
+  start: function () {
+    Log.info("Starting module: " + this.name);
+
+    if (this.shouldLookupGeolocation()) {
+      this.getLocation();
+    }
+    this.scheduleUpdate(this.config.initialLoadDelay);
+  },
+
+  updateWeather: function () {
+    if (this.geoLocationLookupFailed) {
+      return;
+    }
+    if (this.shouldLookupGeolocation() && !this.geoLocationLookupSuccess) {
+      this.scheduleUpdate(1000); // try again in one second
+      return;
+    }
+
+    var units = this.config.unitTable[this.config.units] || 'auto';
+
+    var url = this.config.apiBase+'/'+this.config.apiKey+'/'+this.config.latitude+','+this.config.longitude+'?units='+units+'&lang='+this.config.language;
+    if (this.config.data) {
+      // for debugging
+      this.processWeather(this.config.data);
+    } else {
+      getJSONP(url, this.processWeather.bind(this), this.processWeatherError.bind(this));
+    }
+  },
+
+  processWeather: function (data) {
+    if (this.config.debug) {
+      console.log('weather data', data);
+    }
+    this.loaded = true;
+    this.weatherData = data;
+    this.temp = this.roundTemp(this.weatherData.currently.temperature);
+    this.updateDom(this.config.animationSpeed);
+    this.scheduleUpdate();
+  },
+
+  processWeatherError: function (error) {
+    if (this.config.debug) {
+      console.log('process weather error', error);
+    }
+    // try later
+    this.scheduleUpdate();
+  },
+
+  notificationReceived: function(notification, payload, sender) {
+    switch(notification) {
+      case "DOM_OBJECTS_CREATED":
+        break;
+    }
+  },
+
+  getDom: function() {
+    var wrapper = document.createElement("div");
+
+    if (this.config.apiKey === "") {
+      wrapper.innerHTML = "Please set the correct forcast.io <i>apiKey</i> in the config for module: " + this.name + ".";
+      wrapper.className = "dimmed light small";
+      return wrapper;
+    }
+
+    if (this.geoLocationLookupFailed) {
+      wrapper.innerHTML = "Geolocaiton lookup failed, please set <i>latitude</i> and <i>longitude</i> in the config for module: " + this.name + ".";
+      wrapper.className = "dimmed light small";
+      return wrapper;
+    }
+
+    if (!this.loaded) {
+      wrapper.innerHTML = this.translate('LOADING');
+      wrapper.className = "dimmed light small";
+      return wrapper;
+    }
+
+    var currentWeather = this.weatherData.currently;
+    var hourly         = this.weatherData.hourly;
+
+    var large = document.createElement("div");
+    large.className = "large light";
+
+    var icon = currentWeather ? currentWeather.icon : hourly.icon;
+    var iconClass = this.config.iconTable[icon];
+    var icon = document.createElement("span");
+    icon.className = 'big-icon wi ' + iconClass;
+    large.appendChild(icon);
+
+    var temperature = document.createElement("span");
+    temperature.className = "bright";
+    temperature.innerHTML = " " + this.temp + "&deg;";
+    large.appendChild(temperature);
+
+    var summaryText = hourly.summary;
+    var summary = document.createElement("div");
+    summary.className = "small dimmed summary";
+    summary.innerHTML = summaryText;
+
+    wrapper.appendChild(large);
+    wrapper.appendChild(summary);
+
+    wrapper.appendChild(this.renderWeatherForecast());
+
+    return wrapper;
+  },
+
+  // Get current day from time
+  getDayFromTime: function (time) {
+    var dt = new Date(time * 1000);
+    return moment.weekdaysShort(dt.getDay());
+  },
+
+  // Get current hour from time
+  // Depending on config returns either
+  //  - 23:00 - 24 hour format
+  //  - 11pm  - 12 hour format
+  getHourFromTime: function (time) {
+    var dt = new Date(time * 1000);
+    var hour = dt.getHours();
+    if (this.config.twentyFourHourTime) {
+      return hour + ":00";
+    }
+    else {
+      var twentyFourHourFormat = "am";
+      if (hour > 12) {
+        twentyFourHourFormat = "pm";
+      }
+      return (hour % 12) + twentyFourHourFormat;
+    }
+  },  
+
+  // A bunch of these make up the meat
+  // In each row we can should display
+  //  - time, icon, precip, temp
+  renderForecastRow: function (data) {
+    // Start off with our row
+    var row = document.createElement("tr");
+    row.className = "forecast-row";
+
+    // time - hours
+    var hourTextSpan = document.createElement("span");
+    hourTextSpan.className = "forecast-hour"
+    hourTextSpan.innerHTML = this.getHourFromTime(data.time)
+    
+    // icon
+    var iconClass = this.config.iconTable[data.icon];
+    var icon = document.createElement("span");
+    icon.className = 'wi weathericon ' + iconClass;
+
+    // precipitation
+    // extra check here is due to darksky precip being optional
+    var precipPossibility = document.createElement("span");
+    precipPossibility.innerHTML = "N/A"
+    if (data.precipProbability != null) {
+      precipPossibility.innerHTML = Math.round(data.precipProbability * 100) + "%";
+    }
+    precipPossibility.className = "precipitation"
+
+    // temperature
+    var temp = data.temperature;
+    temp = Math.round(temp);
+    var temperature = document.createElement("span");
+    temperature.innerHTML = temp + "&deg;";
+    temperature.className = "temp";
+
+    // Add what's necessary and return it
+    row.appendChild(hourTextSpan)
+    if (this.config.showIconInRow) { row.appendChild(icon); }
+    if (this.config.showPrecipitationPossibilityInRow) { row.appendChild(precipPossibility) }
+    row.appendChild(temperature)
+
+    return row;
+  },
+
+  renderWeatherForecast: function () {
+    // Placeholders
+    var numHours =  this.config.maxHoursForecast;
+
+    // Truncate for the data we need
+    var filteredHours =
+      this.weatherData.hourly.data.filter( function(d, i) { return (i <= numHours); });
+
+    // Setup what we'll be displaying
+    var display = document.createElement("table");
+    display.className = "forecast";
+
+    var days = [];
+
+    // Cycle through and populate our table
+    for (let i = 1; i < filteredHours.length; i++) {
+      // insert day here if necessary
+      var hourData = filteredHours[i];
+      var row = this.renderForecastRow(hourData);
+
+      let day = this.getDayFromTime(hourData.time);
+      let daySpan = document.createElement("span");
+
+      if (days.indexOf(day) == -1) {
+        daySpan.innerHTML = day;
+        days.push(day);
+      }
+
+      if (this.config.showDayInRow) { row.prepend(daySpan); }
+
+      display.appendChild(row);
+    }
+    
+    return display;
+  },
+
+  getLocation: function () {
+    var self = this;
+    navigator.geolocation.getCurrentPosition(
+      function (location) {
+        if (self.config.debug) {
+          console.log("geolocation success", location);
+        }
+        self.config.latitude  = location.coords.latitude;
+        self.config.longitude = location.coords.longitude;
+        self.geoLocationLookupSuccess = true;
+      },
+      function (error) {
+        if (self.config.debug) {
+          console.log("geolocation error", error);
+        }
+        self.geoLocationLookupFailed = true;
+        self.updateDom(self.config.animationSpeed);
+      },
+      this.config.geoLocationOptions);
+  },
+
+// Round the temperature based on tempDecimalPlaces
+  roundTemp: function (temp) {
+    var scalar = 1 << this.config.tempDecimalPlaces;
+
+    temp *= scalar;
+    temp  = Math.round( temp );
+    temp /= scalar;
+
+    return temp;
+  },
+
+  scheduleUpdate: function(delay) {
+    var nextLoad = this.config.updateInterval;
+    if (typeof delay !== "undefined" && delay >= 0) {
+      nextLoad = delay;
+    }
+
+    var self = this;
+    setTimeout(function() {
+      self.updateWeather();
+    }, nextLoad);
+  }
+
+});
---
+++ README.md
@@ -0,0 +1,161 @@
+# MMM-darksky-hourly
+This an extension for [MagicMirror](https://github.com/MichMich/MagicMirror) that adds localized weather using the [Dark Sky API](https://darksky.net/dev/) (originally Forecast.io) -- the same service that powers the [Dark Sky App](https://darksky.net/app/).
+
+![screenshot](screenshot.png)
+
+This is a heavily modified version of [MMM-forecast-io](https://github.com/dmcinnes/MMM-forecast-io) that aims to present current and hourly information on temperature and likelihood of rain.
+
+## Using the module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+````javascript
+modules: [
+  {
+    module: 'MMM-darksky-hourly',
+    position: 'top_right',  // This can be any of the regions.
+    config: {
+      // See 'Configuration options' for more information.
+      apiKey: 'abcde12345abcde12345abcde12345ab', // Dark Sky API key.
+      latitude:   16.77532,
+      longitude: -3.008265
+    }
+  }
+]
+````
+
+## Configuration options
+
+<table width="100%">
+  <!-- table suffering... -->
+  <thead>
+    <tr>
+      <th>Option</th>
+      <th width="100%">Description</th>
+    </tr>
+  <thead>
+  <tbody>
+    <tr>
+      <td><code>apiKey</code></td>
+      <td>The <a href="https://darksky.net/dev/" target="_blank">Dark Sky API</a> key, which can be obtained by creating an Dark Sky API account.<br>
+        <br> This value is <b>REQUIRED</b>
+      </td>
+    </tr>
+    <tr>
+      <td><code>units</code></td>
+      <td>What units to use. Specified by config.js<br>
+        <br><b>Possible values:</b> <code>config.units</code> = Specified by config.js, <code>default</code> = Kelvin, <code>metric</code> = Celsius, <code>imperial</code> =Fahrenheit
+        <br><b>Default value:</b> <code>config.units</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>twentyFourHourTime</code></td>
+      <td>Whether to use 24-hour format time or not<br>
+        <br><b>Possible values:</b> <code>true</code> = Time presented as XX:00, <code>false</code> = Time present as XXam/pm
+        <br><b>Default value:</b> <code>true</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>showPrecipitationPossibilityInRow</code></td>
+      <td>Show chance of precipitation at each hour<br>
+        <br><b>Possible values:</b> <code>true</code> = Display precipitation possibility, <code>false</code> = Do not display precipitation details
+        <br><b>Default value:</b> <code>true</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>showDayInRow</code></td>
+      <td>Show day that hour occurs in<br>
+        <br><b>Possible values:</b> <code>true</code> = Display day in row, <code>false</code> = Do not display day in row
+        <br><b>Default value:</b> <code>true</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>showIconInRow</code></td>
+      <td>Show weather icon in each hourly row<br>
+        <br><b>Possible values:</b> <code>true</code> = Display icon in row, <code>false</code> = Do not display icon in row
+        <br><b>Default value:</b> <code>true</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>language</code></td>
+      <td>The language of the weather text.<br>
+        <br><b>Possible values:</b> <code>en</code>, <code>nl</code>, <code>ru</code>, etc ...
+        <br><b>Default value:</b> uses value of <i>config.language</i>
+      </td>
+    </tr>
+    <tr>
+      <td><code>updateInterval</code></td>
+      <td>How often does the content needs to be fetched? (Milliseconds)<br>
+        <br>Forecast.io enforces a 1,000/day request limit, so if you run your mirror constantly, anything below 90,000 (every 1.5 minutes) may require payment information or be blocked.<br>
+        <br><b>Possible values:</b> <code>1000</code> - <code>86400000</code>
+        <br><b>Default value:</b> <code>600000</code> (10 minutes)
+      </td>
+    </tr>
+    <tr>
+      <td><code>animationSpeed</code></td>
+      <td>Speed of the update animation. (Milliseconds)<br>
+        <br><b>Possible values:</b><code>0</code> - <code>5000</code>
+        <br><b>Default value:</b> <code>2000</code> (2 seconds)
+      </td>
+    </tr>
+    <tr>
+      <td><code>initialLoadDelay</code></td>
+      <td>The initial delay before loading. If you have multiple modules that use the same API key, you might want to delay one of the requests. (Milliseconds)<br>
+        <br><b>Possible values:</b> <code>1000</code> - <code>5000</code>
+        <br><b>Default value:</b>  <code>0</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>retryDelay</code></td>
+      <td>The delay before retrying after a request failure. (Milliseconds)<br>
+        <br><b>Possible values:</b> <code>1000</code> - <code>60000</code>
+        <br><b>Default value:</b>  <code>2500</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>latitude</code></td>
+      <td>The latitude location in decimal. Set this (and <code>longitude</code>) as the location for the forecast. If this is not set, the module will attempt to approximate using browser geolocation.<br>
+        <br><b>Example value:</b> <code>16.77532</code>
+        <br><b>Default value:</b>  <code>null</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>longitude</code></td>
+      <td>The longitude location in decimal. Set this (and <code>latitude</code>) as the location for the forecast. If this is not set, the module will attempt to approximate using browser geolocation.<br>
+        <br><b>Example value:</b> <code>-3.008265</code>
+        <br><b>Default value:</b>  <code>null</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>apiBase</code></td>
+      <td>The Dark Sky API base URL.<br>
+        <br><b>Default value:</b>  <code>'https://api.darksky.net/forecast'</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>maxHoursForecast</code></td>
+      <td>Limit how many hours of weather forecast.<br>
+        <br><b>Default value:</b>  <code>8</code>
+      </td>
+    </tr>
+    <tr>
+      <td><code>iconTable</code></td>
+      <td>The conversion table to convert the weather conditions to weather-icons.<br>
+        <br><b>Default value:</b><pre>iconTable: {
+      'clear-day':           'wi-day-sunny',
+      'clear-night':         'wi-night-clear',
+      'rain':                'wi-rain',
+      'snow':                'wi-snow',
+      'sleet':               'wi-rain-mix',
+      'wind':                'wi-cloudy-gusts',
+      'fog':                 'wi-fog',
+      'cloudy':              'wi-cloudy',
+      'partly-cloudy-day':   'wi-day-cloudy',
+      'partly-cloudy-night': 'wi-night-alt-cloudy',
+      'hail':                'wi-hail',
+      'thunderstorm':        'wi-thunderstorm',
+      'tornado':             'wi-tornado'
+    }</pre>
+      </td>
+    </tr>
+  </tbody>
+</table>
